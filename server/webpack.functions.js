9bf46883d69a39997b1d58f69548b838eda04d8c
raghava annala <annalaraghava0@gmail.com>
2025-03-16 15:53:27 +0530

    Fix build configuration for Netlify deployment

---
+++ server/webpack.functions.js
@@ -0,0 +1,22 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                targets: {
+                  node: '14'
+                }
+              }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+}; 
